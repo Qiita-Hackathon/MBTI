f5486ede8b5b43dd0eb8918d31741423c88f0b8c
Tetsu-is <hap30305@kwansei.ac.jp>
2024-02-11 00:04:47 +0900

    rewrite user_table

--- initdb.d/create-table.sql
+++ initdb.d/create-table.sql
@@ -3,22 +3,25 @@ SET CHARACTER SET utf8mb4;
 
 -- ユーザーテーブル
 create table users (
-    id int auto_increment primary key, -- id（主キー）
+    user_id int auto_increment primary key, -- id（主キー）
     user_name varchar(255) not null unique, -- ユーザー名（一意キーにしても良い）
-    display_name varchar(255) not null, -- ユーザー名（一意キーにしても良い）
     email varchar(255) not null unique, -- メールアドレス（同じく一意キーにしても良い）
     password varchar(255) not null, -- パスワード（ハッシュ化してもそのままでも良し）
-    description varchar(255) default '', -- 説明
+    age int not null,
+    gender int not null,
+    occupation varchar(255) default '',
+    self_introduction varchar(255) default '',
     icon_path varchar(255) default '' -- アイコン画像のパス
+    mbti int not null,
 );
 
 -- ユーザーの初期データ追加
-insert into users (user_name, display_name, email, password, icon_path) values 
-('Yusuke', 'ユースケ', 'tanaka@email.com', SHA2('password', 256), ''), -- パスワードをハッシュ化して保存
-('toku', 'とく', 'toto@email.com', SHA2('password', 256), '/public/image/2.png'), -- アイコンのURL保存する場合
-('Umi', '海', 'umiumi@email.com', SHA2('password', 256), ''),
-('Lucky', 'Lucky', 'Lucky@email.com', SHA2('password', 256), ''),
-('Kazuki', '一樹', 'kazu@email.com', SHA2('password', 256), '');
+insert into users (user_name, email, password, age, gender, mbti) values
+('Yusuke', 'tanaka@email.com', SHA2('password', 256), 25, 1, 1),
+('toku', 'tanaka@email.com', SHA2('password', 256), 25, 1, 1),
+('Umi',  'tanaka@email.com', SHA2('password', 256), 25, 1, 0),
+('Lucky', 'tanaka@email.com', SHA2('password', 256), 25, 1, 0),
+('Kazuki', 'tanaka@email.com', SHA2('password', 256), 25, 1, 1);
 
 -- アクセストークンの管理テーブル
 CREATE TABLE access_tokens (
