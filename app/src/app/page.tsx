8371de8210c39ccecb119f5a5808f79c0af5fe94
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-11 03:05:12 +0900

    ホーム画面のモック作成

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,16 +1,45 @@
-import { Button, Center, Container, Heading } from "@yamada-ui/react";
+import { Layout } from "@/components/common/layout";
+import { Box, Heading, Text } from "@yamada-ui/react";
 import type { FC } from "react";
+
+const mock_data = [
+	{
+		userName: "hoge",
+		mbti: "探検家",
+		tags: ["ゲーム", "食べ歩き", "映画"],
+	},
+	{
+		userName: "hoge",
+		mbti: "探検家",
+		tags: ["ゲーム", "食べ歩き", "映画"],
+	},
+	{
+		userName: "hoge",
+		mbti: "探検家",
+		tags: ["ゲーム", "食べ歩き", "映画"],
+	},
+];
 
 const Home: FC = () => {
 	return (
-		<Container>
-			<Center flexDir="column" gap={2}>
-				<Heading>Hello World</Heading>
-				<Button as="a" href="https://yamada-ui.com" target="_blank">
-					Click Here!
-				</Button>
-			</Center>
-		</Container>
+		<Layout>
+			{mock_data.map((item, index) => (
+				<Box key={index} p={3}>
+					<Heading size="lg">{item.userName}</Heading>
+					<Text>MBTI: {item.mbti}</Text>
+					<Box>
+						<Text size="1rem">
+							タグ:{" "}
+							{item.tags.map((e, i) => (
+								<Box as="span" key={i} px={2}>
+									{e}
+								</Box>
+							))}
+						</Text>
+					</Box>
+				</Box>
+			))}
+		</Layout>
 	);
 };
 
