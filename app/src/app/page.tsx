6988d6af7da003e3cd56728576e176d798cfdf8a
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-11 13:10:03 +0900

    feat: プロフィール一覧取得のAPI取得

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,43 +1,53 @@
+"use client";
 import { Layout } from "@/components/common/layout";
-import { Box, Heading, Text } from "@yamada-ui/react";
-import type { FC } from "react";
+import { Badge, Box, Card, HStack, Heading, Text } from "@yamada-ui/react";
+import { useState, type FC, useEffect } from "react";
 
-const mock_data = [
-	{
-		userName: "hoge",
-		mbti: "探検家",
-		tags: ["ゲーム", "食べ歩き", "映画"],
-	},
-	{
-		userName: "hoge",
-		mbti: "探検家",
-		tags: ["ゲーム", "食べ歩き", "映画"],
-	},
-	{
-		userName: "hoge",
-		mbti: "探検家",
-		tags: ["ゲーム", "食べ歩き", "映画"],
-	},
-];
+interface CategoryTag {
+	tagId: number;
+	categoryGroup: string;
+	tagName: string;
+}
+
+interface Users {
+	id: number;
+	userName: string;
+	userIcon: string;
+	mbti: string;
+	categoryTag: CategoryTag[];
+}
 
 const Home: FC = () => {
+	const [profiles, setProfiles] = useState<Users[]>([]);
+
+	const firstFetch = async () => {
+		const response = await fetch(
+			"http://localhost:8080/api/profile/all?mbtiId=1",
+		);
+		const { profiles } = await response.json();
+		setProfiles(profiles);
+		console.log(profiles);
+	};
+
+	useEffect(() => {
+		firstFetch();
+	}, [firstFetch]);
+
 	return (
 		<Layout>
-			{mock_data.map((item, index) => (
-				<Box key={`${index}-${item.userName}`} p={3}>
+			{profiles.map((item, index) => (
+				<Card key={`${index}-${item.userName}`} p={3}>
 					<Heading size="lg">{item.userName}</Heading>
 					<Text>MBTI: {item.mbti}</Text>
-					<Box>
-						<Text size="1rem">
-							タグ:{" "}
-							{item.tags.map((e, i) => (
-								<Box as="span" key={`${i}-${e}`} px={2}>
-									{e}
-								</Box>
-							))}
-						</Text>
-					</Box>
-				</Box>
+					<HStack gap={2} fontSize="1rem">
+						<Box>タグ: </Box>
+						{item.categoryTag.map((e, i) => (
+							<Badge as="span" key={`${i}-${e.tagId}`} px={2}>
+								{e.categoryGroup}: {e.tagName}
+							</Badge>
+						))}
+					</HStack>
+				</Card>
 			))}
 		</Layout>
 	);
